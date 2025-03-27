e05c822497d55f8425bfe0b1d8acac939a0d5f83
MohamedMoustir <itsmoustir@gmail.com>
2025-03-27 11:28:21 +0000

    int les evants candidatuer

---
+++ app/Events/CandidatureStatusUpdated.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Events;
+
+use App\Models\Candidature;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CandidatureStatusUpdated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+    public $candidature;
+    public function __construct(Candidature $candidature)
+    {
+        $this->candidature = $candidature;
+    }
+}
